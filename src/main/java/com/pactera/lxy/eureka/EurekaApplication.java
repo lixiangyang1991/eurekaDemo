d891f196e456c86833ecc1b679d3d582198b32ef
lixiangyang <lixiangyang_1110@163.com>
2019-11-11 09:34:15 +0800

    我今天很开心

--- src/main/java/com/pactera/lxy/eureka/EurekaApplication.java
+++ src/main/java/com/pactera/lxy/eureka/EurekaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer//测试git666
+//我再提交一次试试
 public class EurekaApplication {
 
 	public static void main(String[] args) {
